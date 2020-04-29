dc9cd87c75c8876783b012dee3f2143b0d8cd1f7
tomtitherington <tom.titherington@gmail.com>
2020-04-29 18:21:28 +0100

    stop point identification

--- stop_point_detection.py
+++ stop_point_detection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import numpy as np
 import pandas as pd
+import datetime
 
 '''
 dataframe format:
@@ -9,7 +10,6 @@ taxi id, date time, longitude, latitude
 
 
 def mean_coordinate(longs, lats, size):
-    # int i = 0 ; i < size ; i+=1
     avg_longs = 0
     avg_lats = 0
     for i, j in zip(longs, lats):
@@ -17,10 +17,12 @@ def mean_coordinate(longs, lats, size):
         avg_lats += j
     return (avg_longs / size, avg_lats / size)
 
+
 ''' time difference in minutes '''
-def timespan(p_jt, p_it):
-    (p_jt-p_it).astype('timedelta64[m]')
-    return 0
+
+
+def timespan(p_it, p_jt):
+    return ((p_jt - p_it).seconds) / 60
 
 
 '''φ is latitude, λ is longitude, R is earth’s radius (mean radius = 6,371km)
@@ -46,32 +48,34 @@ def distance(p_i, p_j):
 
 def stop_point_detection(df, df_size, dist_thresh, time_thresh):
     i = 0
-    stop_points = {'taxi_id': [], 'date_time': [],
+    stop_points = {'taxi_id': [], 'arrival_dt': [], 'departure_dt': [],
                    'longitude': [], 'latitude': []}
     while i < df_size:
         j = i + 1
         while j < df_size:
+            print(df.iat[j, 2], df.iat[j, 3])
             dist = distance((df.iat[i, 2], df.iat[i, 3]),
                             (df.iat[j, 2], df.iat[j, 3]))
             if dist > dist_thresh:
                 delta_t = timespan(df.iat[i, 1], df.iat[j, 1])
                 if delta_t > time_thresh:
                     stop_points['taxi_id'].append(df.iat[i, 0])
-                    # split into arrival and leaving time?
-                    stop_points['date_time'].append(df.iat[i, 1])
+                    stop_points['arrival_dt'].append(df.iat[i, 1])
+                    stop_points['departure_dt'].append(df.iat[j, 1])
                     (long, lat) = mean_coordinate(
-                        df.iloc[i:j, 2], df.iloc[i:j, 3], j - i + 1)
+                        df.iloc[i:j + 1, 2], df.iloc[i:j + 1, 3], j - i + 1)
                     stop_points['longitude'].append(long)
                     stop_points['latitude'].append(lat)
                 i = j
                 break
             j += 1
+        i += 1
 
     return pd.DataFrame(stop_points)
 
 
-#print(distance((116.51172, 39.92123), (116.51135, 39.93883)))
 df = pd.read_csv('release/taxi_log_2008_by_id/1.txt', header=None, names=[
                  "taxi_id", "date_time", "longitude", "latitude"])
-timespan(df.iloc[0,1],df.iloc[1,1])
-#print(mean_coordinate(df.iloc[0:3, 2], df.iloc[0:3, 3], 3 - 0 + 1))
+df['date_time'] = pd.to_datetime(df['date_time'])
+# distnace threshold 50 meters, time threshold 3 minutes
+print(stop_point_detection(df, len(df.index), 50, 3))
